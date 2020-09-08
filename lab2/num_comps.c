b8fcc2aa05645b4c258dbc43003ac701bb485c04
Reet Tiwary <rtiwary2@illinois.edu>
2020-09-07 21:54:48 -0500

    Updated with last exercise

--- lab2/num_comps.c
+++ lab2/num_comps.c
@@ -1,13 +1,13 @@
 
 # include <stdio.h>
 
-int main()
+int  main()
 {
-  int val1 = -22;
-  int val2 = 33;
-  int res;
+  int  val1 = -11;
+ int  val2 = 3;
+  int  res;
    
-  res = val1 * val2;
+  res = val1 %  val2;
   
   printf("%d\n", res);
  
